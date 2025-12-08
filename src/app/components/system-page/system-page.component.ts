450c7f3300bc32d66a87c3c5385932e44da7e9da
Daniel Huglen <daniel.holmehuglen@lyse.no>
2025-12-08 14:47:11 +0100

    Add outcomes section

--- src/app/components/system-page/system-page.component.ts
+++ src/app/components/system-page/system-page.component.ts
@@ -1,3 +1,4 @@
+import { NgClass, NgTemplateOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, effect, signal } from '@angular/core';
 import { form } from '@angular/forms/signals';
 
@@ -11,8 +12,77 @@ interface ExperienceFormData {
 	templateUrl: './system-page.component.html',
 	styleUrl: './system-page.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [NgClass, NgTemplateOutlet],
 })
 export class SystemPageComponent {
+	protected isCriticalSuccessSelected = signal(false);
+	protected isSuccessSelected = signal(false);
+	protected isMixedSelected = signal(false);
+	protected isFailureSelected = signal(false);
+	protected isCriticalFailureSelected = signal(false);
+
+	isButtonDisabled(selectedButton: string): boolean {
+		switch (selectedButton) {
+			case 'criticalSuccess':
+				return (
+					this.isSuccessSelected() ||
+					this.isMixedSelected() ||
+					this.isFailureSelected() ||
+					this.isCriticalFailureSelected()
+				);
+			case 'success':
+				return (
+					this.isCriticalSuccessSelected() ||
+					this.isMixedSelected() ||
+					this.isFailureSelected() ||
+					this.isCriticalFailureSelected()
+				);
+			case 'mixed':
+				return (
+					this.isCriticalSuccessSelected() ||
+					this.isSuccessSelected() ||
+					this.isFailureSelected() ||
+					this.isCriticalFailureSelected()
+				);
+			case 'failure':
+				return (
+					this.isCriticalSuccessSelected() ||
+					this.isSuccessSelected() ||
+					this.isMixedSelected() ||
+					this.isCriticalFailureSelected()
+				);
+			case 'criticalFailure':
+				return (
+					this.isCriticalSuccessSelected() ||
+					this.isSuccessSelected() ||
+					this.isMixedSelected() ||
+					this.isFailureSelected()
+				);
+			default:
+				return false;
+		}
+	}
+
+	get outcomeText(): string {
+		if (this.isCriticalSuccessSelected()) {
+			return 'Agents performing beyond expectations are often rewarded generously. This extra reward sometimes manifests as extra gold or reputation, but more often means they bring back a magic item, potion, new member, or even plot-relevant information from their adventure!';
+		}
+		if (this.isSuccessSelected()) {
+			return 'The rewards for this outcome is exactly what was promised when embarking on the mission. A positive amount of gold and reputation.';
+		}
+		if (this.isMixedSelected()) {
+			return 'When not everything goes according to plan, the employer might be reluctant to award the guild their promised rewards, considering the outcome a breach of contract. Some gold will most likely be given to the agents, though the reputation will suffer, sometimes even moving into the negatives!';
+		}
+		if (this.isFailureSelected()) {
+			return 'Failing a mission means no rewards. For a few missions, it even means the guild will owe gold to the employer. Either way, the guild reputation is certain to take a hit.';
+		}
+		if (this.isCriticalFailureSelected()) {
+			return 'The worst possible outcome requires intervention by the party. There will be different levels of detriment though, as some issues could be solved by casting a simple spell, while others might require travelling to a different city and responding to threats face-to-face. Regardless, negative gold and reputation is to be expected.';
+		}
+
+		return 'No Outcome Selected';
+	}
+
 	experienceModel = signal<ExperienceFormData>({
 		level: null,
 		missionResult: null,
